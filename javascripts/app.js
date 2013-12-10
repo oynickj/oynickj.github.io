c4f792af45febe051b7f4ab8ead35a7a95323dd6
Josh Oynick <oynickj@gmail.com>
2013-12-10 15:33:43 -0500

    commented out javascript

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,25 +1,31 @@
 $(document).ready(function(){
 
-  $("#projects").on("click", function(e){
-    e.preventDefault();
-    $.ajax({
-      // local event
-      beforeSend: function(){
-        $("#repos").addClass("loading")
-      },
-      success: function(response){
-        $.each(response, function(index, repo){
-          $("#repos").append('<ul>');
-          $("#repos").find('ul').append('<li>' + repo.name + '</li>');
-          console.log(repo.name);
-          });
-      },
-      complete: function(){
-        $("#repos").removeClass("loading")
-      },
-      url: "https://api.github.com/users/oynickj/repos"
-    });
-  })
+
+
+
+
+
+
+  // $("#projects").on("click", function(e){
+  //   e.preventDefault();
+  //   $.ajax({
+  //     // local event
+  //     beforeSend: function(){
+  //       $("#repos").addClass("loading")
+  //     },
+  //     success: function(response){
+  //       $.each(response, function(index, repo){
+  //         $("#repos").append('<ul>');
+  //         $("#repos").find('ul').append('<li>' + repo.name + '</li>');
+  //         console.log(repo.name);
+  //         });
+  //     },
+  //     complete: function(){
+  //       $("#repos").removeClass("loading")
+  //     },
+  //     url: "https://api.github.com/users/oynickj/repos"
+  //   });
+  // })
 
 
 
