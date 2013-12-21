a6c559bc2d4e26120e9f7230d3d8e876c6ab9527
Josh Oynick <oynickj@gmail.com>
2013-12-21 13:49:44 -0500

    added analytics

--- javascripts/app.js
+++ javascripts/app.js
@@ -23,30 +23,4 @@ $(document).ready(function(){
 
   });
 
-  // todo: fix movement to top of page on click of contact
-
-
-  // $("#projects").on("click", function(e){
-  //   e.preventDefault();
-  //   $.ajax({
-  //     // local event
-  //     beforeSend: function(){
-  //       $("#repos").addClass("loading")
-  //     },
-  //     success: function(response){
-  //       $.each(response, function(index, repo){
-  //         $("#repos").append('<ul>');
-  //         $("#repos").find('ul').append('<li>' + repo.name + '</li>');
-  //         console.log(repo.name);
-  //         });
-  //     },
-  //     complete: function(){
-  //       $("#repos").removeClass("loading")
-  //     },
-  //     url: "https://api.github.com/users/oynickj/repos"
-  //   });
-  // })
-
-
-
 });
